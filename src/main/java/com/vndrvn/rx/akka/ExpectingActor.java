f61dd0e943b61b60fcc0f283e5f6736e0edd8648
Andrew VanderVeen <avanderveen@ea.com>
2016-01-16 12:39:00 -0500

    Call onComplete for ActorObservables when the corresponding Actor is stopped

--- src/main/java/com/vndrvn/rx/akka/ExpectingActor.java
+++ src/main/java/com/vndrvn/rx/akka/ExpectingActor.java
@@ -5,6 +5,8 @@ import akka.actor.Props;
 import akka.japi.pf.FI;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.Optional;
+
 public class ExpectingActor<T> extends AbstractActor {
 
 	public static <U> Props props(final ObservableExpectation<U> expectation) {
@@ -15,7 +17,10 @@ public class ExpectingActor<T> extends AbstractActor {
 		return Props.create(ExpectingActor.class, expectation);
 	}
 
+	protected final Optional<ObservableExpectation<T>> expectation;
+
 	protected ExpectingActor(final ObservableExpectation<T> expectation) {
+		this.expectation = Optional.of(expectation);
 		receive(ReceiveBuilder
 				.match(expectation.getMessageClass(), expectation::onNext)
 				.matchAny(any -> expectation.onError(new UnexpectedActorMessageException(any)))
@@ -23,6 +28,7 @@ public class ExpectingActor<T> extends AbstractActor {
 	}
 
 	protected ExpectingActor(final SingleExpectation<T> expectation) {
+		this.expectation = Optional.empty();
 		receive(ReceiveBuilder
 				.match(expectation.getMessageClass(), stopAfter(expectation::onSuccess))
 				.matchAny(stopAfter(any -> expectation.onError(new UnexpectedActorMessageException(any))))
@@ -34,6 +40,13 @@ public class ExpectingActor<T> extends AbstractActor {
 			consumer.apply(message);
 			context().stop(self());
 		};
+	}
+
+	@Override
+	public void postStop() {
+		if (expectation.isPresent()) {
+			expectation.get().onCompleted();
+		}
 	}
 
 }
